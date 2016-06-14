8c6ff3c6c1f42662a11b35024dcebafc13368ecd
Antonio Jesús Sánchez Padial <antonio.sanchez@inia.es>
2016-06-14 12:53:22 +0200

    Draft serapeo.py

---
+++ serapeo.py
@@ -0,0 +1,29 @@
+from werkzeug.wsgi import SharedDataMiddleware
+import os
+from werkzeug.wrappers import Request, Response
+
+class Serapeo(object):
+
+    def dispatch_request(self, request):
+        return Response('Hello World!')
+
+    def wsgi_app(self, environ, start_response):
+        request = Request(environ)
+        response = self.dispatch_request(request)
+        return response(environ, start_response)
+
+    def __call__(self, environ, start_response):
+        return self.wsgi_app(environ, start_response)
+
+def create_app(with_static=True):
+    app = Serapeo()
+    if with_static:
+        app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+            '/static': os.path.join(os.path.dirname(__file__), 'static')
+        })
+    return app
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    app = create_app()
+    run_simple('127.0.0.1', 5000, app, use_debugger=False, use_reloader=True)
