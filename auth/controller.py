b84538bfeffc813eab8d77fff58f2d8ef1a2edd4
Antonio Jesús Sánchez Padial <antonio@spadial.com>
2016-06-16 14:52:03 +0200

    Refactor login/user features into its own module.

---
+++ auth/controller.py
@@ -0,0 +1,24 @@
+import plugins
+import flask
+
+def _get(request):
+    login_plugins = map ((lambda p: p.create_plugin()), plugins.get_auth_plugins())
+    return flask.render_template('login.html', login_plugins = login_plugins)
+
+def _post(request):
+    plugin_name = request.form['auth_plugin']
+    auth_plugin = plugins.get_auth_plugin(plugin_name)
+    if auth_plugin == False:
+        return _get(request)
+    else:
+        result = auth_plugin.create_plugin().validate(request.form)
+        if result == False:
+             return 'Login error'
+        else:
+             return 'User: ' + result
+
+def login(request):
+    if request.method == 'GET':
+        return _get(request)
+    if request.method == 'POST':
+        return _post(request)
--- serapeo.py
+++ serapeo.py
@@ -1,31 +1,12 @@
 import plugins
 import flask
+import auth.controller
 
 app = flask.Flask(__name__)
 
 @app.route("/login", methods=['GET', 'POST'])
 def on_login():
-    error = None
-    url = ''
-    if flask.request.method == 'GET':
-        return get_login()
-    if flask.request.method == 'POST':
-        plugin_name = flask.request.form['auth_plugin']
-        auth_plugin = plugins.get_auth_plugin(plugin_name)
-        if auth_plugin == False:
-            return get_login()
-        else:
-            result = auth_plugin.create_plugin().validate(flask.request.form)
-            if result == False:
-                 return 'Login error'
-            else:
-                 return 'User: ' + result
-            
-
-def get_login():
-        login_plugins = map ((lambda p: p.create_plugin()), plugins.get_auth_plugins())
-        return flask.render_template('login.html', login_plugins = login_plugins)
-
+    return auth.controller.login(flask.request)
 
 if __name__ == '__main__':
     app.run()
